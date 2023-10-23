bbe1a11ba951006277d5506fd79134460e6d4465
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-10-23 12:14:50 +0530

    Delete routes directory

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-const router =express.Router();
-const admindata= require('../controllers/admin')
-
-//create hotel and room
-router.post('/createhotel',admindata.createHotel)
-router.post('/createroom/:id',admindata.createroom)
-
-module.exports=router;
